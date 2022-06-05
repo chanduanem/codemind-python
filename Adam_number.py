ef22d97380550baeafbade029acc6ab855805307
chanduanem <106660132+chanduanem@users.noreply.github.com>
2022-06-05 14:06:41 +0530

    def reverse(n):     rev=0     while n>0:         r=n%10         rev=rev*10+r         n=n//10     return rev a=int(input()) c=a**2 v=reverse(a) p=reverse(v**2) if c==p:     print('True') else:     print('False')

--- Adam_number.py
+++ Adam_number.py
@@ -1,10 +1,15 @@
 def reverse(n):
-    rev=0   
+    rev=0
     while n>0:
         r=n%10
         rev=rev*10+r
         n=n//10
     return rev
 a=int(input())
-print(a**2==reverse(reverse(a)**2))
-        
+c=a**2
+v=reverse(a)
+p=reverse(v**2)
+if c==p:
+    print('True')
+else:
+    print('False')
